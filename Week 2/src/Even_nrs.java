3ded15cfd19e4778b3b76beaec4ee8368f097114
Georg <georg-edward.piil@voco.ee>
2026-09-10 09:27:21 +0300

    Ülesanne29: Even numbers

---
+++ Week 2/src/Even_nrs.java
@@ -0,0 +1,9 @@
+public class Even_nrs {
+    public static void main(String[] args) {
+
+        for (int number = 2; number <= 100; number = number + 2) {
+            System.out.println(number);
+        }
+
+    }
+}
