27f6366847d06677812cd7ba6ba959dc7c80348b
Georg <georg-edward.piil@voco.ee>
2026-09-19 19:30:04 +0300

    Ülesanne 6: kahe arvu liitmine

---
+++ Week 1/src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int number1 = 34;
+        int number2 = 56;
+        int sum = number1 + number2;
+
+        System.out.println(number1 + "+" + number2 + "=" + sum);
+    }
+}
